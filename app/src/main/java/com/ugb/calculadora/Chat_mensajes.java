3459f49dd1afd3f4850a1fe33a54c18d9965a7ae
Rodriguez <elbacristinarodriguezperdomo@gmail.com>
2020-12-01 16:45:17 -0600

    nueva clase

---
+++ app/src/main/java/com/ugb/calculadora/Chat_mensajes.java
@@ -0,0 +1,12 @@
+package com.ugb.calculadora;
+
+public class Chat_mensajes {
+    public boolean posicion;
+    public String message;
+
+    public Chat_mensajes(boolean posicion, String message) {
+        super();
+        this.posicion = posicion;
+        this.message = message;
+    }
+}
